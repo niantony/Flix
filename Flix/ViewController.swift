378626150e249fd28915965c11f145a08d638bd5
niantony <66891025+niantony@users.noreply.github.com>
2022-01-23 17:51:09 -0500

    images fixed

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -59,7 +59,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        let baseUrl = "http://image.tmdb.org/t/p/w185"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
 
