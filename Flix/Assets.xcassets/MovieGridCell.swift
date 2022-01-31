b1d509cf8dd4955bcc1515885c7f8db708b0d49f
niantony <66891025+niantony@users.noreply.github.com>
2022-01-30 20:16:22 -0500

    Part 2

---
+++ Flix/Assets.xcassets/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Antony Ni on 1/30/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
